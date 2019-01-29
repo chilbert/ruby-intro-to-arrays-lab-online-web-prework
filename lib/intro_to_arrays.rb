baa6142b3658d2252b41d9d40fb642aa21a18e7b
Chase Hilbert <chase@heyspacetime.com>
2019-01-29 05:22:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,8 +18,6 @@ def last_element(array)
   my_last_element = array[-1]
 end
 
-def first_element_with_array_methods
-
+def first_element_with_array_methods(array)
   first_country = array.first
-
 end
